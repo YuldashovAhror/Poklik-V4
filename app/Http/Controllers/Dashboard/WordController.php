0a9cc6935814624975d2bbfffc6fb53580df5c60
YuldashovAhror <ahroryuldashov123@gmail.com>
2023-05-14 12:53:44 +0500

    first commit

---
+++ app/Http/Controllers/Dashboard/WordController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+
+class WordController extends Controller
+{
+    public function index()
+    {
+        dd('asd');
+    }
+}
